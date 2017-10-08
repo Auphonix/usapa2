fe1177f6c3201ab47fb5395560f8080a4bcf5309
Danyon <s3547463@student.rmit.edu.au>
2017-10-08 20:10:53 +1100

    Updated the default password

--- users.pp
+++ users.pp
@@ -1,5 +1,8 @@
 class user1{
 
+    # HASH FOR THE DEFAULT PASSWORD 'password'
+    $def_hash = '$6$kq22ZJbo$m6uJHRzuBTYJA4tXasjjoml/M9zIco3ht9cNGJ0/x1QYuZQZz0zo2PgUWIxTDzuEh932cLsF.kjup3h1Rs/Gj.'
+
     file { '/home/becca':
     # Ensure becca's home directory is created
     ensure => 'directory',
@@ -15,7 +18,7 @@ class user1{
     groups => ['sysadmin', 'cars'],
 
     # Allow the user time to change their password
-    password => sha1('password'), #enctypts with local encryption
+    password => $def_hash, #enctypts with local encryption
     password_min_age => '1',
     password_max_age => '7',
 
