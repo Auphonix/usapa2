df62795149a8c7a40875154c0691e1111b53b31c
Danyon <s3547463@student.rmit.edu.au>
2017-10-08 17:07:38 +1100

    Updated USers

--- users.pp
+++ users.pp
@@ -1,11 +1,27 @@
-class users{
+class user1{
     user { 'becca'
     ensure => 'present',
+
+    # Set her home directory
     home => '/home/becca',
 
     # Allow the user time to change their password
     password => sha1('password'),
     password_min_age => '1',
     password_max_age => '7',
+
+    # Set UID
+    uid => '10017463'
+
+    # Set shell
+    shell => '/bin/bash'
+    }
+
+    group { 'sysadmin'
+    ensure => 'present',
+    }
+
+    group { 'cars'
+    ensure => 'present'
     }
 }
