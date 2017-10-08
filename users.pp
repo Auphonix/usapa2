1efa87137d84e5ceafcdde14e01fb3bf58d99916
Danyon <s3547463@student.rmit.edu.au>
2017-10-08 17:36:27 +1100

    Fixed home directory not existing for becca

--- users.pp
+++ users.pp
@@ -1,12 +1,21 @@
 class user1{
+
+    file { '/home/becca':
+    # Ensure becca's home directory is created
+    ensure => 'directory',
+    }
+
     user { 'becca':
     ensure => 'present',
 
     # Set her home directory
     home => '/home/becca',
 
+    # Add to groups
+    groups => ['sysadmin', 'cars'],
+
     # Allow the user time to change their password
-    password => sha1('password'),
+    password => '!!',
     password_min_age => '1',
     password_max_age => '7',
 
