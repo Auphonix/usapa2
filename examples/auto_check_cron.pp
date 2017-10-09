33515776d581cf37f5fe0ce5f39cec443005c14b
Danyon <s3547463@student.rmit.edu.au>
2017-10-10 10:32:19 +1100

    Added cronjob

--- examples/auto_check_cron.pp
+++ examples/auto_check_cron.pp
@@ -1,3 +1,7 @@
+
+# Terminal command
+#puppet resource cron puppet-agent ensure=present user=root minute=*/20 command='/opt/puppetlabs/bin/puppet agent --onetime --no-daemonize --splay --splaylimit 60'
+
 cron { 'puppet-agent':
   ensure  => 'present',
   command => '/opt/puppetlabs/bin/puppet agent --onetime --no-daemonize --splay --splaylimit 60',
