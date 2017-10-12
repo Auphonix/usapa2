e1617ee08d9fcb3bd1b9277e5be5bb8c2ca2dafc
Danyon <s3547463@student.rmit.edu.au>
2017-10-12 23:14:35 +1100

    Chaged document root for apache

--- file_config.pp
+++ file_config.pp
@@ -1,8 +1,28 @@
+class httpd_root_setup{
+
+    # Ensure root directory exists
+    file { "/var/www/s3547463":
+        ensure => 'directory',
+    }
+
+    augeas { "httpd_conf":
+      context => "/files/etc/httpd/conf/httpd_conf",
+      changes => [
+        "set DocumentRoot /var/www/s3547463",
+      ],
+    }
+
+}
+
 class file_config{
+    # Configure ssh root
     augeas { "sshd_config":
       context => "/files/etc/ssh/sshd_config",
       changes => [
         "set PermitRootLogin no",
       ],
     }
+
+    # Configure httpd root directory
+    httpd_root_setup
 }
