851dc7bea6410dfcfefad3fae569d0dd2e7fc61b
Danyon <s3547463@student.rmit.edu.au>
2017-10-13 04:02:51 +1100

    Fixed variable replacement

--- scripts/sudoers_fixup.sh
+++ scripts/sudoers_fixup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i -e '311d' /opt/puppetlabs/puppet/share/augeas/lenses/dist/sudoers.aug
 
-sed -i $content /opt/puppetlabs/puppet/share/augeas/lenses/dist/sudoers.aug
+sed -i '311i \\t\t\t | "umask_override" | "use_pty" | "match_group_by_gid"' /opt/puppetlabs/puppet/share/augeas/lenses/dist/sudoers.aug
