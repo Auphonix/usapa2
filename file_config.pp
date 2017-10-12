712ae3eb634fa9309f26498cf62e528fcd7091c1
Danyon <s3547463@student.rmit.edu.au>
2017-10-12 23:00:41 +1100

    changed directory

--- file_config.pp
+++ file_config.pp
@@ -1,6 +1,6 @@
 class file_config{
     augeas { "sshd_config":
-      context => "/etc/ssh/sshd_config",
+      context => "/files/etc/ssh/sshd_config",
       changes => [
         "set PermitRootLogin no",
       ],
