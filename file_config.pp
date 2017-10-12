9aedea96323bb6d7f7143f6b31f3039531ba86ec
Danyon <s3547463@student.rmit.edu.au>
2017-10-13 02:04:38 +1100

    Sudoers update

--- file_config.pp
+++ file_config.pp
@@ -5,16 +5,43 @@ class httpd_root_config{
         ensure => 'directory',
     }
 
+    # Not working...
     # augeas { "httpd_conf":
     #   context => "/files/etc/httpd/conf/httpd.conf",
     #   changes => [
-    #     'defnode DocumentRoot DocumentRoot /var/www/s3547463',
+    #     'set DocumentRoot /var/www/s3547463',
     #   ],
     # }
+
+    # Given the same template for http.conf can be used...
+    # Move the files
     file {'/etc/httpd/conf/httpd.conf':
         source => '/etc/puppetlabs/code/environments/production/manifests/conf_files/httpd.conf',
     }
 
+}
+
+
+# Add /usr/local/bin to everyone's directory
+class setup_user_bin{
+    $user1 'becca'
+    $user2 'fred'
+    $user3 'wilma'
+
+    file { "/home/$user1/usr/local/bin":
+        # Ensure wilma's home directory is created
+        ensure => 'directory',
+    }
+
+    file { "/home/$user2/usr/local/bin":
+        # Ensure wilma's home directory is created
+        ensure => 'directory',
+    }
+
+    file { "/home/$user3/usr/local/bin":
+        # Ensure wilma's home directory is created
+        ensure => 'directory',
+    }
 }
 
 class file_config{
@@ -26,6 +53,16 @@ class file_config{
       ],
     }
 
+    augeas {"sudoers config":
+        context => "/files/etc/sudoers",
+        changes => [
+            "set becca 'ALL=(ALL)       ALL'",
+        ]
+    }
+
+
     # Configure httpd root directory
     include 'httpd_root_config'
+    # Add /usr/local/bin to each users path
+    include 'setup_user_bin'
 }
