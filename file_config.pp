7b3ee818b6f209d3cfefe63a7b3bd754c9207fff
Danyon <s3547463@student.rmit.edu.au>
2017-10-12 23:59:55 +1100

    httpd update

--- file_config.pp
+++ file_config.pp
@@ -8,8 +8,7 @@ class httpd_root_config{
     augeas { "httpd_conf":
       context => "/files/etc/httpd/conf/httpd.conf",
       changes => [
-        'set directive[.="DocumentRoot"] "/var/www/s3547463"',
-        "set directive[.='DocumentRoot']/arg '${controller_name}'",
+        'set "DocumentRoot" "/var/www/s3547463"',
       ],
     }
 
